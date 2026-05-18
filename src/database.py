feb883a1dace241fd372f7091e3d668281177f1c
Almaz Kisi <103516578+toleshee2015@users.noreply.github.com>
2026-05-18 17:43:11 +0300

    Update database.py

--- src/database.py
+++ src/database.py
@@ -1,12 +1,4 @@
-import psycopg2
+from sqlalchemy import create_engine
 
-DB_CONFIG = {
-    "host": "localhost",
-    "database": "bank_reviews",
-    "user": "postgres",
-    "password": "123",
-    "port": 5432
-}
-
-def get_connection():
-    return psycopg2.connect(**DB_CONFIG)
+def get_engine():
+    return create_engine("postgresql://user:password@localhost:5432/bank_reviews")
