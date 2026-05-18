1b9058e9c8586708f2ac1b1f76123d939ac48b66
Almaz Kisi <103516578+toleshee2015@users.noreply.github.com>
2026-05-18 20:33:38 +0300

    Create db_check.py

---
+++ src/db_check.py
@@ -0,0 +1,15 @@
+from sqlalchemy import text
+from src.database import get_engine
+
+def check_db():
+    engine = get_engine()
+
+    with engine.connect() as conn:
+        result = conn.execute(
+            text("SELECT COUNT(*) FROM reviews")
+        )
+
+        print(result.fetchone())
+
+if __name__ == "__main__":
+    check_db()
