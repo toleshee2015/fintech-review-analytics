5cfa399b319d5d05bb2820ba23957b8d94a329c4
Almaz Kisi <103516578+toleshee2015@users.noreply.github.com>
2026-05-18 17:44:44 +0300

    Update database.py

--- src/database.py
+++ src/database.py
@@ -1,4 +1,18 @@
 from sqlalchemy import create_engine
+from sqlalchemy import text
+from src. database import get_engine
 
+def check_db():
+    engine = get_engine()
+
+    with engine.connect() as conn:
+        result = conn.execute(
+            text("SELECT COUNT(*) FROM reviews")
+        )
+
+        print(result.fetchone())
+
+if __name__ == "__main__":
+    check_db()
 def get_engine():
     return create_engine("postgresql://user:password@localhost:5432/bank_reviews")
