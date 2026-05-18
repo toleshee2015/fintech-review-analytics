696bbd1df31d4ad151c48da269026ac88de00c26
Almaz Kisi <103516578+toleshee2015@users.noreply.github.com>
2026-05-18 21:08:29 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,22 +1,14 @@
-CREATE TABLE IF NOT EXISTS banks (
+CREATE TABLE banks (
     bank_id SERIAL PRIMARY KEY,
     bank_name VARCHAR(100) UNIQUE NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS reviews (
+CREATE TABLE reviews (
     review_id SERIAL PRIMARY KEY,
-    bank_id INTEGER NOT NULL,
+    bank_id INTEGER REFERENCES banks(bank_id),
     review_text TEXT NOT NULL,
     rating INTEGER CHECK (rating BETWEEN 1 AND 5),
-    review_date DATE,
-
     sentiment_label VARCHAR(20),
-    sentiment_score NUMERIC(5,4),
-    identified_theme VARCHAR(100),
-    language VARCHAR(10),
-
-    CONSTRAINT fk_bank
-        FOREIGN KEY (bank_id)
-        REFERENCES banks(bank_id)
-        ON DELETE CASCADE
+    sentiment_score NUMERIC,
+    identified_theme VARCHAR(100)
 );
