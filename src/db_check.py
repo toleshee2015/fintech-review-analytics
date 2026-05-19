1757d4564d44b9fc34e1cee03e1a0e430ffe834a
Almaz Kisi <103516578+toleshee2015@users.noreply.github.com>
2026-05-19 16:47:33 +0300

    Update db_check.py

--- src/db_check.py
+++ src/db_check.py
@@ -1,1 +1,13 @@
 
+from src.database import get_engine
+import pandas as pd
+
+def check_connection():
+    engine = get_engine()
+
+    with engine.connect() as conn:
+        print("Connection OK")
+        print(conn.execute("SELECT 1").fetchone())
+
+if __name__ == "__main__":
+    check_connection()
